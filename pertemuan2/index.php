6db9a05eb5f6df099442af503df8378558ffa3e2
Rachmawati09 <chyntiacitra04@gmail.com>
2021-02-11 13:54:22 +0700

    menyelesaikan pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,66 @@
+<?php
+/*
+    Chyntia Citra Rachmawati
+    203040002
+    github.com/Rachmawati09/pw2021_203040002
+    Pertemuan 2 (8 februari 2021)
+    Materi minggu ini mempelajari mengenai penggunaan sintaks Php Dasar
+*/
+?>
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks 
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variable dan Tipe Data
+// Variable 
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Chyntia Citra";
+// echo "Hai, Nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x + $y;
+
+// penggabung string / congcatenation / concat
+// . 
+// $nama_depan = "Chyntia";
+// $nama_belakang = "Citra";
+// echo $nama_depan . " " . $nama_belakang;
+
+// assigment
+// =, +=, -=, *=, /=, %=, .= 
+// $x = 1;
+// $x += 5;
+// echo $x;
+// $nama = "Chyntia";
+// $nama .= " ";
+// $nama .= "Citra";
+// echo $nama;
+
+// perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// logika
+// &&, ||, ! 
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+?>
+
+
