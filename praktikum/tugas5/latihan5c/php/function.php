adc5f971f280c6c155f64ea55a9624b12129f307
Rachmawati09 <75963270+Rachmawati09@users.noreply.github.com>
2021-04-21 00:18:48 +0700

    menyelesaikan tugas 5
    menyelesaikan tugas 5

---
+++ praktikum/tugas5/latihan5c/php/function.php
@@ -0,0 +1,45 @@
+<?php
+function koneksi()
+{
+    $conn = mysqli_connect("localhost", "root", "");
+    mysqli_select_db($conn, "pw_tubes_203040002");
+
+    return $conn;
+}
+
+function query ($sql)
+{
+    $conn = koneksi();
+    $result = mysqli_query($conn, "$sql");
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data)
+{
+    $conn = koneksi();
+
+    $Nama = htmlspecialchars($data['Nama Produk']);
+    $Kegunaan = htmlspecialchars($data['Kegunaan']);
+    $Harga = htmlspecialchars($data['Harga']);
+    $Gambar = htmlspecialchars($data['Gambar']);
+
+    $query = "INSERT INTO skincare
+                    VALUES
+                    ('', '$Gambar', '$Kegunaan', '$Nama', '$Harga')";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function hapus($No)
+{
+    $conn = koneksi();
+    mysqli_query($conn, "DELETE FROM skincare WHERE id = $No");
+
+    return mysqli_affected_rows($conn);
+}
